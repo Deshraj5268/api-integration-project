72f3b98efcbc48c44f683cc144c7166841e9541e
deshraj.thakur <deshrajnitbhopal.com>
2020-07-08 08:45:51 +0530

    Deshraj|Api integration

---
+++ api-integration/src/main/java/raj/integration/apiintegration/ApiIntegrationApplication.java
@@ -0,0 +1,20 @@
+package raj.integration.apiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ApiIntegrationApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ api-integration/src/main/java/raj/integration/apiintegration/controller/ApiConsumerController.java
@@ -0,0 +1,42 @@
+package raj.integration.apiintegration.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ApiConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/test")
+    public List<String> test(){
+        String url = "http://localhost:8991/countries";
+        String [] objects = restTemplate.getForObject(url,String[].class);
+        //List<String> list = Arrays.asList(objects);
+        return  Arrays.asList(objects);
+    }
+    @GetMapping("/testing")
+    public String testing(){
+        return "success";
+    }
+
+    @GetMapping("/countries")
+    public List<String> getCountries(){
+        List<String> countriesList = new ArrayList<>();
+        countriesList.add("india");
+        countriesList.add("US");
+        countriesList.add("UK");
+        countriesList.add("AU");
+        return countriesList;
+    }
+}
